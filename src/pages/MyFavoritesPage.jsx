31819470d5bf8e7280bc1b96a0f8801a237e4924
Claire Lacanal <lacanal.claire@gmail.com>
2024-06-02 00:11:49 +0200

    page favorites done

--- src/pages/MyFavoritesPage.jsx
+++ src/pages/MyFavoritesPage.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+import apiHandler from "../utils/apiHandler";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+
 function MyFavoritePage() {
+  const [favorites, setFavorites] = useState(
+    () => JSON.parse(localStorage.getItem("favorites")) || []
+  );
+  const [annonces, setAnnonces] = useState([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        const allAnnonces = await apiHandler.getAllAnnonces();
+        const filteredAnnonces = allAnnonces.data.filter((annonce) =>
+          favorites.includes(annonce._id)
+        );
+        setAnnonces(filteredAnnonces);
+      } catch (error) {
+        console.error("Erreur pour afficher les annonces favorites", error);
+      }
+    };
+
+    fetchFavorites();
+  }, [favorites]); // Ajouter `favorites` comme dépendance pour recharger à la mise à jour
+
+  const handleRemoveFavorite = (id) => {
+    const newFavorites = favorites.filter((favId) => favId !== id);
+    setFavorites(newFavorites);
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+  };
+
   return (
     <>
-      <h1>Ma page de favoris</h1>
+      <h1>Mes annonces favorites</h1>
+      <div>
+        {annonces.map((annonce) => (
+          <div key={annonce._id}>
+            <h3>{annonce.kind}</h3>
+            <p>{annonce.photo}</p>
+            <p>{annonce.city}</p>
+            <p>{annonce.description}</p>
+            <p>
+              Du {annonce.startDate} au {annonce.endDate}
+            </p>
+            <FontAwesomeIcon
+              icon={faHeartSolid}
+              onClick={() => handleRemoveFavorite(annonce._id)}
+              style={{ color: "red", cursor: "pointer" }}
+            />
+          </div>
+        ))}
+        {annonces.length === 0 && <p>Aucune annonce favorite sélectionnée.</p>}
+      </div>
     </>
   );
 }
